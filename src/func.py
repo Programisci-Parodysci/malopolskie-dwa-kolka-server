340a126bcf71b9aa34930c8b5316ee756181dcf2
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-09-28 23:17:03 +0200

    mapping x to location

--- src/func.py
+++ src/func.py
@@ -2,24 +2,27 @@
 from ast import arg
 import requests
 import sys
+import asyncio
+from geopy.geocoders import Nominatim
+
+app = Nominatim(user_agent="malopolskie_dwa_kola")
 
 def get_address(lat, lon, params):
     url='https://nominatim.openstreetmap.org/reverse?lat=<value>&lon=<value>&<params>'
-    url.replace(f'lat=<value>', f'lat=<{lat}>')
-    url.replace(f'lon=<value>', f'lon=<{lon}>')
-    if params != "":
-        url.replace(f'&<params>', f'&<{params}>')
-    else:
-        url.replace(f'&<params>', f'')
+    url=url.replace(f'lat=<value>', f'lat={lat}')
+    url=url.replace(f'lon=<value>', f'lon={lon}')
+    url=url.replace(f'&<params>', f'')
+    print(url)
     response=requests.get(url)
     print(response)
-    # content=response.json()
-    # print(content)
+    content=response.content
+    print(content)
 
 if __name__=='__main__':
-    lat=sys.argv[0]
-    lon=sys.argv[1]
+    lat=sys.argv[1]
+    lon=sys.argv[2]
     params=""
-    if len(sys.argv)>2:
-        params=sys.argv[2]
+    if len(sys.argv)>3:
+        params=sys.argv[3]
+        print('params:', params)
     get_address(lat, lon, params)
