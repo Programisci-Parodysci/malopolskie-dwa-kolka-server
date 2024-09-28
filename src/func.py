bcf127d29f05ee2acb82c42ce8c8e423a743d57c
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-09-28 23:34:14 +0200

    work on auto suggestion

--- src/func.py
+++ src/func.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import asyncio
 from geopy.geocoders import Nominatim
+import json
 
 app = Nominatim(user_agent="malopolskie_dwa_kola")
 
@@ -12,17 +13,24 @@ def get_address(lat, lon, params):
     url=url.replace(f'lat=<value>', f'lat={lat}')
     url=url.replace(f'lon=<value>', f'lon={lon}')
     url=url.replace(f'&<params>', f'')
-    print(url)
     response=requests.get(url)
-    print(response)
     content=response.content
     print(content)
 
+
+def get_suggestions(letters):
+    url='https://nominatim.openstreetmap.org/search?<params>'
+    url=url.replace(f'<params>', f'q={letters}')
+    response=requests.get(url)
+    content=response.content
+    print(response)
+    print(content)
 if __name__=='__main__':
-    lat=sys.argv[1]
-    lon=sys.argv[2]
-    params=""
-    if len(sys.argv)>3:
-        params=sys.argv[3]
-        print('params:', params)
-    get_address(lat, lon, params)
+    # lat=sys.argv[1]
+    # lon=sys.argv[2]
+    # params=""
+    # if len(sys.argv)>3:
+    #     params=sys.argv[3]
+    #     print('params:', params)
+    # get_address(lat, lon, params)
+    get_suggestions('Kra, małopolska')
