6409885c8508608b2e8a54ee17fd9fa22d074d1b
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-09-29 09:48:46 +0200

    temp fix for logging

--- src/func.py
+++ src/func.py
@@ -5,7 +5,7 @@ import sys
 import asyncio
 from geopy.geocoders import Nominatim
 import json
-from log import logger
+# from logs import logger
 
 app = Nominatim(user_agent="malopolskie_dwa_kola")
 
@@ -18,7 +18,7 @@ def get_address(lat, lon, params):
         response=requests.get(url)
         content=response.content
     except Exception as e:
-        logger.error(e)
+        # logger.error(e)
         return ""
     return content
 
@@ -30,41 +30,34 @@ def get_suggestions(letters):
         response=requests.get(url)
         content=response.content
     except Exception as e:
-        logger.error(e)
+        # logger.error(e)
         return ""
     return content
 
 def get_suggestions_photon(letters):
     '''zalecam podanie nazwy województwa po przecinku tzn. Lesser Poland Voivodeship'''
-    logger.info(f'letters: {letters}')
+    # logger.info(f'letters: {letters}')
     url='https://photon.komoot.io/api/?<params>'
     url=url.replace(f'<params>', f'q={letters}')
-    logger.debug(f'url: {url}')
+    # logger.debug(f'url: {url}')
     try:
         response=requests.get(url)
         content=response.content
     except Exception as e:
-        logger.error(e)
+        # logger.error(e)
         return ""
     return content
 
 def get_coordinates_from_address(address):
     '''zwraca krotkę (lat, lon)'''
-    logger.info(f'address: {address}')
+    # logger.info(f'address: {address}')
     try:
         location = app.geocode(address)
-        logger.debug(f'location: {location}')
+        # logger.debug(f'location: {location}')
     except Exception as e:
-        logger.error(e)
+        # logger.error(e)
         return "", ""
     return location.latitude, location.longitude
 
 if __name__=='__main__':
-    # lat=sys.argv[1]
-    # lon=sys.argv[2]
-    # params=""
-    # if len(sys.argv)>3:
-    #     params=sys.argv[3]
-    #     print('params:', params)
-    # get_address(lat, lon, params)
-    get_coordinates_from_address('Kraków, Lesser Poland Voivodeship')
+    print(get_suggestions_photon('Krak, Lesser Poland Voivodeship'))
